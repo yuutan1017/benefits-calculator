7a165e2683c17324ee88cfb4e8b4de7e580ffb76
yuutan1017 <n.you0611@gmail.com>
2022-08-25 18:28:24 +0900

    愚直にuseStateで配列作成

--- pages/AveCalculate.tsx
+++ pages/AveCalculate.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState, useRef, RefObject } from 'react';
 import Link from 'next/link';
 
 import { Layout } from '../components/Layout';
 
 const AveCalculate = () => {
+  const [one, setOne] = useState<string>("");
+  const [two, setTwo] = useState<string>("");
+  const [three, setThree] = useState<string>("");
+  const [four, setFour] = useState<string>("");
+  const [five, setFive] = useState<string>("");
+  const [six, setSix] = useState<string>("");
+
+  let data: number[] = [];
+  const handlePushCalculate = () => {
+    data.push(parseInt(one));
+    data.push(parseInt(two));
+    data.push(parseInt(three));
+    data.push(parseInt(four));
+    data.push(parseInt(five));
+    data.push(parseInt(six));
+    console.log(data);
+  };
+
   return (
     <>
       <Layout>
@@ -13,36 +31,49 @@ const AveCalculate = () => {
             <form className="mt-8">
               <input
                 type="number"
+                value={one}
+                onChange={(e) => setOne(e.target.value)}
                 className="bg-gray-100 border-b-2 border-gray-500 text-gray-800 text-sm focus:outline-none block w-full p-2 placeholder:text-right"
                 placeholder="円"
               />
               <input
                 type="number"
+                value={two}
+                onChange={(e) => setTwo(e.target.value)}
                 className="bg-gray-100 border-b-2 border-gray-500 text-gray-800 text-sm focus:outline-none block w-full p-2 mt-4 placeholder:text-right"
                 placeholder="円"
               />
               <input
                 type="number"
+                value={three}
+                onChange={(e) => setThree(e.target.value)}
                 className="bg-gray-100 border-b-2 border-gray-500 text-gray-800 text-sm focus:outline-none block w-full p-2 mt-4 placeholder:text-right"
                 placeholder="円"
               />
               <input
                 type="number"
+                value={four}
+                onChange={(e) => setFour(e.target.value)}
                 className="bg-gray-100 border-b-2 border-gray-500 text-gray-800 text-sm focus:outline-none block w-full p-2 mt-4 placeholder:text-right"
                 placeholder="円"
               />
               <input
                 type="number"
+                value={five}
+                onChange={(e) => setFive(e.target.value)}
                 className="bg-gray-100 border-b-2 border-gray-500 text-gray-800 text-sm focus:outline-none block w-full p-2 mt-4 placeholder:text-right"
                 placeholder="円"
               />
               <input
                 type="number"
+                value={six}
+                onChange={(e) => setSix(e.target.value)}
                 className="bg-gray-100 border-b-2 border-gray-500 text-gray-800 text-sm focus:outline-none block w-full p-2 mt-4 placeholder:text-right"
                 placeholder="円"
               />
               <button
                 type="button"
+                onClick={handlePushCalculate}
                 className="focus:outline-none text-white bg-green-700 hover:bg-green-800 font-normal rounded text-md px-5 py-3 mt-5 w-full"
               >
                 平均値を計算する
