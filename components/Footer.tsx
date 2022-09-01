cd72e64ba33cb92a701c1a5d80a495b16e7757fc
yuutan1017 <n.you0611@gmail.com>
2022-09-01 20:20:17 +0900

    table

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <footer className="w-full h-14 flex justify-center items-center border-t-4 pt-5 mb-7">
-      <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-700 sm:mb-0">
+    <footer className="w-full h-10 flex justify-center items-center border-t-2 pt-5 mb-7">
+      <ul className="flex flex-wrap items-center text-sm text-gray-700 sm:mb-0">
         <li>
           <a href="#" className="mr-4 hover:underline sm:mr-8 sm:text-lg">
             About me
--- pages/ChildcareCalculate.tsx
+++ pages/ChildcareCalculate.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import { Layout } from '../components/Layout';
 
 const ChildcareCalculate = () => {
@@ -6,34 +7,52 @@ const ChildcareCalculate = () => {
     <>
       <Layout>
         <section>
-          <h1 className="text-xl font-bold">
-            あなたの育児休業給付金、自動で計算します。
-          </h1>
+          <section className="mx-6">
+            <h1 className="text-xl font-bold">
+              あなたの育児休業給付金、自動で計算します。
+            </h1>
+            <div className="text-md text-left mt-8">
+              <p>
+                労働者が子育てするために休業する場合、育児休業給付金が受け取れます。
+              </p>
+              <p>
+                また、
+                <b>育児休業期間中は社会保険料などの納付が免除されます。</b>
+              </p>
+            </div>
+          </section>
           <section className="mx-6 mt-5 text-left">
-            <div className='text-md'>
-              <p>&lt;初月から6ヵ月まで&gt;</p>
+            <div className="text-md">
+              <p className="mt-3">&lt;初月から6ヵ月まで&gt;</p>
               <p className="mt-1">
-                ・育児休業開始から180日： （休業開始時賃金日額 × 支給日数
+                ・育児休業開始から180日：（休業開始時賃金日額<sup>※1</sup> ×
+                支給日数
                 <sup>※2</sup>）× 67%
               </p>
-              <p className="mt-2">&lt;6ヵ月以降&gt;</p>
+              <p className="mt-1">&lt;6ヵ月以降&gt;</p>
               <p className="mt-1">
-                ・育児休業開始から181日目以降：（休業開始時賃金日額 × 支給日数
+                ・育児休業開始から181日目以降：（休業開始時賃金日額<sup>※1</sup>{' '}
+                × 支給日数
                 <sup>※2</sup>）× 50%
               </p>
             </div>
-            <div className="mt-2 text-stone-500 text-sm">
+            <div className="mt-3 text-stone-400 text-sm">
               <p>
                 ※1：育児休業開始前（産休を取った場合は産休開始前）6カ月間の賃金を180で割った額
               </p>
-              <p>※2：通常は30日</p>
+              <Link href="/AveCalculate">
+                <p className="w-52 hover:font-bold hover:text-gray-900 hover:cursor-pointer">
+                  －－&gt;平均賃金の計算はこちらから
+                </p>
+              </Link>
+              <p className="mt-1">※2：通常は30日</p>
             </div>
           </section>
-          <article className="flex flex-col py-4 mx-8 mt-6 bg-gray-200">
+          <article className="flex flex-col py-4 mx-6 mt-6 bg-gray-200 border-t-2 border-indigo-300">
             <div className="flex flex-row justify-center">
               <span>毎月の額面給与</span>
               <input
-                className="border-b-2 border-indigo-400 w-32 ml-2"
+                className="border-b-2 border-gray-400 w-32 ml-2"
                 type="text"
               />
             </div>
@@ -41,8 +60,9 @@ const ChildcareCalculate = () => {
               <span>開始日程</span>
               <input
                 type="date"
+                value="2022-05-13"
                 required
-                className="border-b-2 border-indigo-400 w-32 ml-2"
+                className="border-b-2 border-gray-400 w-32 ml-2"
               />
             </div>
           </article>
