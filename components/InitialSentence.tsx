eb8161bfd9e0b33f992ef6ab8cf4893c3d72ae12
yuutan1017 <n.you0611@gmail.com>
2022-09-08 19:22:15 +0900

    text

--- components/InitialSentence.tsx
+++ components/InitialSentence.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const InitialSentence = () => {
   return (
     <>
-      <article className="mx-4 mt-10">
+      <article className="mx-4 mt-16">
         <h1 className="text-2xl font-bold">
           あなたの育児休業給付金、自動で計算します。
         </h1>
-        <div className="text-md text-left mt-10">
+        <div className="text-md text-left mt-12">
           <p>
             労働者が子育てするために休業する場合、育児休業給付金が受け取れます。
           </p>
           <p>
             また、
-            <b>育児休業期間中は社会保険料などの納付が免除されます。</b>
+            <b className="border-b-4 border-yellow-400">
+              育児休業期間中は社会保険料などの納付が免除されます。
+            </b>
           </p>
         </div>
-        <div className="mt-6 mb-2 text-left">
+        <div className="mt-8 mb-2 text-left">
           赤ちゃんが産まれる時にもらえるお金や育児休業給付金をもらう条件など
           <br />
           詳しく知りたい方はこちらをご覧ください。
@@ -26,20 +29,20 @@ export const InitialSentence = () => {
           <div className="flex flex-row hover:text-blue-400">
             <Link href="https://ninps.com/workstyle/work/archives/29">
               <a>
-                参考 ：
+                参考 :
                 育児休業給付金とは？申請・計算方法や延長できるケースまで、どこよりもわかりやすく解説
               </a>
             </Link>
           </div>
         </div>
       </article>
-      <section className="mx-4 mt-5 text-left border-t-2 border-[#39497C]">
+      <section className="mx-4 mt-8 text-left border-t-4 border-[#39497C]">
         <section>
           <div className="flex flex-row items-center text-center">
-            <span className="font-bold text-xl text-center mr-2 mt-4 text-yellow-500">
+            <span className="font-bold text-xl text-center mr-2 mt-8 text-yellow-500">
               ||
             </span>
-            <h2 className="mt-4 text-xl">計算方法について</h2>
+            <h2 className="mt-8 text-xl">計算方法について</h2>
           </div>
           <div className="text-sm">
             <p className="mt-5">&lt;初月から6ヶ月まで&gt;</p>
@@ -66,8 +69,9 @@ export const InitialSentence = () => {
             <p className="mt-1">※2：通常は30日</p>
           </div>
         </section>
+
         <section>
-          <div className="flex flex-row mt-10">
+          <div className="flex flex-row mt-16">
             <span className="font-bold text-xl text-center mr-2 text-yellow-500">
               ||
             </span>
@@ -75,24 +79,58 @@ export const InitialSentence = () => {
           </div>
           <div className="text-sm mt-8">
             <p>
-              ・<b>月10日以内かつ80時間以内での勤務</b>
+              育児休業中は本来、就労することは想定されていません。事業主側から求めに応じ、合意することで就労することはできますが、
+            </p>
+            <p>
+              あくまで
+              <b className="border-b-4 border-yellow-400">
+                「臨時的かつ一時的」
+              </b>
+              なものでなくてはなりません。
             </p>
             <p className="mt-4">
-              であれば支給されますが、収入額によっては次のような支給の調整が行われます。　
+              詳しく知りたい場合はお勤め先の人事やハローワークにてご確認下さい。
             </p>
-            <p className="mt-8">&lt;支給額の調整パターン&gt;</p>
-            <div className="border-4 border-yellow-500 p-5 mt-3">
-              <p className="p-1">・満額支給</p>
-              <p className="ml-5 text-gray-600">
-                ⇒給与額が、賃金月額の１３％（または３０％）まで
+            <div className="text-[12px] text-gray-500 mt-2">
+              <div className="flex flex-row hover:text-blue-400">
+                <Link href="https://we-love.gunma.jp/area/childcareleave-half">
+                  <a>
+                    参考 :
+                    半育休の働き方とは？育休中に働く場合の収入や注意点を解説
+                  </a>
+                </Link>
+              </div>
+            </div>
+            <p className='mt-8'>
+              以上を踏まえて、就労できる日数や時間が下記のように決まっています。
+            </p>
+            <div className="mt-12">
+              <p className="font-bold">・月10日以内の就労である</p>
+              <p className="font-bold mt-3">
+                ・月10日以上の就労の場合は、1ヶ月の就労時間が80時間以内であること
               </p>
-              <p className="p-1 mt-2">・支給額の調整</p>
-              <p className="ml-5 text-gray-600">
-                ⇒給与額が、賃金月額の１３％（または３０％）超え、８０％未満
+              <p className="font-bold mt-3">
+                ・「週◯回」「毎週◯曜日」など、定期的な就労でないこと
               </p>
-              <p className="p-1 mt-2">・支給停止</p>
-              <p className="ml-5 text-gray-600">
-                ⇒給与額が、賃金月額の８０％を超える場合
+              <p className="mt-12">
+                以上の条件を満たしていないと、育休中でないとみなされ、育児休業給付金の支給が止まるおそれがあります。　
+              </p>
+              <p>なお、収入額によっては次のような支給の調整が行われます。</p>
+            </div>
+
+            <p className="mt-10">&lt;支給額の調整パターン&gt;</p>
+            <div className="border-2 border-yellow-500 p-5 mt-5">
+              <p className="p-1 text-gray-700">・満額支給</p>
+              <p className="ml-4 font-bold">
+                → 給与額が、賃金月額の１３％（または３０％）まで
+              </p>
+              <p className="p-1 mt-2 text-gray-700">・支給額の調整</p>
+              <p className="ml-4 font-bold">
+                → 給与額が、賃金月額の１３％（または３０％）超え、８０％未満
+              </p>
+              <p className="p-1 mt-2 text-gray-700">・支給停止</p>
+              <p className="ml-4 font-bold">
+                → 給与額が、賃金月額の８０％を超える場合
               </p>
             </div>
           </div>
