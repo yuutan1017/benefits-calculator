b1bd0b2b7354f7d4dfeb536c0ddc30fa050c848a
yuutan1017 <n.you0611@gmail.com>
2022-09-19 21:02:21 +0900

    link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Link as Scroll } from 'react-scroll';
+import { Link } from 'react-scroll';
 
 export const Navbar = () => {
+  const scroll = () => {
+    window.scrollTo({
+      behavior: 'smooth',
+    });
+  };
   return (
     <header className="w-full flex justify-center items-center border-t-2 pt-5 mr-24 md:mr-96 mb-3">
       <ul className="flex flex-wrap items-center text-sm sm:mb-0 font-bold">
         <li className="mr-4 sm:mr-8 text-[#13184d] sm:text-lg cursor-pointer">
-          <Scroll to="methodCalculate" duration={500} offset={50} smooth>
+          <Link to="methodCalculate" duration={500} offset={50} smooth>
             計算方法
-          </Scroll>
+          </Link>
         </li>
         <li className="mr-4 sm:mr-8 text-[#13184d] sm:text-lg cursor-pointer">
-          <Scroll to="aboutWork" duration={500} offset={50} smooth>
+          <Link to="aboutWork" duration={500} offset={50} smooth>
             就労について
-          </Scroll>
+          </Link>
         </li>
         <li className="sm:mr-16 text-[#13184d] sm:text-lg cursor-pointer">
-          <Scroll to="contact" duration={500} offset={50} smooth>
+          <Link to="contact" duration={500} offset={50} smooth>
             お問い合わせ
-          </Scroll>
+          </Link>
         </li>
       </ul>
     </header>
