06dce46bffdb193bbe1ccfd2ab6a1b5f1b49ad56
yuutan1017 <n.you0611@gmail.com>
2022-09-20 22:34:16 +0900

    twitter

--- next-seo.config.js
+++ next-seo.config.js
@@ -13,13 +13,4 @@ export default {
     locale: 'ja_JP',
     site_name: '育児休業給付金計算ツール',
   },
-  twitter: {
-    title: '育休中に働いた場合はどうなるか、自動でシミュレーションします',
-    description:
-      '就労をする場合の育児休業給付金計算ツール、お金のことに焦点を絞って作りました',
-    handle: '@NYou0611',
-    site: '@NYou0611',
-    cardType: 'summary_large_image',
-    image: '',
-  },
 };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -8,8 +14,14 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='ja'>
-        <Head></Head>
+      <Html lang="ja">
+        <Head>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="2@NYou0611" />
+          <meta name="twitter:title" content="育休中に働いた場合はどうなるか、自動でシミュレーションします" />
+          <meta name="twitter:description" content="主に、給与や給付金などお金のことについてまとめています。" />
+          <meta name="twitter:image" content="https://paternity-leave.vercel.app/images/top.png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
