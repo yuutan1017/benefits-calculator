97284d2a210e81368869936ff3ca9935acd338cd
yuutan1017 <n.you0611@gmail.com>
2022-09-24 23:54:25 +0900

    build

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,10 +5,10 @@ export const Footer = () => {
   return (
     <footer className="w-full flex justify-center items-center border-t-2 pt-5 mb-7">
       <ul className="flex flex-wrap items-center text-sm text-gray-700 sm:mb-0">
-
-      
-        <li className='ml-6'>
-          <p className="sm:text-lg font-light text-gray-500">copyright@yuutan1017</p>
+        <li className="ml-6">
+          <p className="sm:text-lg font-light text-gray-500">
+            copyright@yuutan1017
+          </p>
         </li>
       </ul>
     </footer>
